459c269a3224301d7b0f487cfa6b4c5c68042daa
Ricardo13433433 <113352215+Ricardo13433433@users.noreply.github.com>
2023-09-20 09:46:13 -0300

    Add files via upload

--- b/Media.js
+++ b/Media.js
@@ -1,3 +1,10 @@
 const form = document.querySelector("form")
 const resp1= doument.querySelector("h3")
 const resp2= document.querySelector("h4")
+form.addEventListener("submit",(e) =>{
+    e.preventDefault()
+    const nome=form.inNome.value
+    const nota1=Number(form.inNota1.value)
+    const nota2=Number(form.inNota2.value)
+    const media=(nota1+nota2)/2
+    resp1.innerText='Media das Notas ${media.toFixed(2)}'
