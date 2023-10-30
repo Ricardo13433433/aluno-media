577912c5fd5fbfe6d5ece5cb0f05e38312f5a221
Ricardo13433433 <113352215+Ricardo13433433@users.noreply.github.com>
2023-10-30 08:19:29 -0300

    30/10

--- b/contas do mes.js
+++ b/contas do mes.js
@@ -5,18 +5,6 @@ const resp2= document.querySelector("h4")
 form.addEventListener("submit",(e) =>{
     e.preventDefault()
 
-    const nome=form.inNome.value
-    const nota1=Number(form.inNota1.value)
-    const nota2=Number(form.inNota2.value)
-    const media=(nota1+nota2)/2
-    resp1.innerText= `Media das Notas ${media.toFixed(2)}`
-if(media>=7) {
-        resp2.innerText=`Parabéns $ {nome}!Voçe foi aprovado(a)`
-        resp2.style.color="blue"
-} else if (media>=4) {
-    resp2.innertText=`Atençao ${nome}! Voçe esstá de exame`
-    resp2.style.color
-}
-
+  
     
 })
