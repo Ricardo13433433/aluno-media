4d34944f48edc8ab213213e9ebfec715d05e40ea
Ricardo13433433 <113352215+Ricardo13433433@users.noreply.github.com>
2023-11-22 10:01:09 -0300

    Add files via upload

---
+++ b/recuperaçao/farmacia.js
@@ -0,0 +1,31 @@
+const frm =- document.querySelector("form")
+const resp1 = document.querySelector("outResp1")
+const resp2 = document.querySelector("outResp2")
+
+
+frm.addEventListener("submit", (e) =>{
+    e.preventDefult()
+
+    cont valor = Number (frm.inValor.value)
+    resp1.innerText ='Valor da compra : R${valor.toFixed(2)}'
+
+
+
+    let valParcelas
+    let numParcela = Math.floor(valor / 20)
+
+    if(numParcelas > 6) {
+        valParcelas = valor / 6
+        resp2.innerText='Pode pagar em: 6 x de : R$(valParcelas.toFixed(2)}'    
+     }  else if(numParcelas <=1){
+
+     } else {
+        valParcelas = valor/numParcelas
+        resp2.innerText = 'Pode pagar em: ${numParcelas} x de R$ ${valParcelas.toFixed(2)}'
+     }
+
+    
+})
+frm.addEventListener("reset", (e) =>) {
+    resp1.innerText = 
+}
